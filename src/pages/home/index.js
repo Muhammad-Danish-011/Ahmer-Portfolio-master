d07768eda0b80068ce458cf19a05ea6d8b2dbad6
Muhammad-Danish-011 <softwareengineer.danish@gmail.com>
2026-01-07 18:18:45 +0500

    updated

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -6,48 +6,71 @@ import { introdata, meta } from "../../content_option";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
+  // Create array of all animated texts from introdata
+  const animatedTexts = [
+    introdata.animated.first,
+    introdata.animated.second,
+    introdata.animated.third,
+    introdata.animated.fourth,
+    introdata.animated.fifth,
+    introdata.animated.sixth
+  ].filter(text => text); // Filter out any undefined/null values
+
   return (
     <HelmetProvider>
       <section id="home" className="home">
         <Helmet>
           <meta charSet="utf-8" />
-          <title> {meta.title}</title>
+          <title>{meta.title}</title>
           <meta name="description" content={meta.description} />
+          <meta name="keywords" content={meta.keywords} />
         </Helmet>
-        <div className="intro_sec d-block d-lg-flex align-items-center ">
+        <div className="intro_sec d-block d-lg-flex align-items-center">
           <div
-            className="h_bg-image order-1 order-lg-2 h-100 "
-            style={{ backgroundImage: `url(${introdata.your_img_url})` }}
-
+            className="h_bg-image order-1 order-lg-2 h-100"
+            style={{ 
+              backgroundImage: `url(${introdata.your_img_url})`,
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat'
+            }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
-            <div className="align-self-center ">
+            <div className="align-self-center">
               <div className="intro mx-auto">
                 <h2 className="mb-1x">{introdata.title}</h2>
                 <h1 className="fluidz-48 mb-1x">
                   <Typewriter
                     options={{
-                      strings: [
-                        introdata.animated.first,
-                        introdata.animated.second,
-                        introdata.animated.third,
-                      ],
+                      strings: animatedTexts,
                       autoStart: true,
                       loop: true,
-                      deleteSpeed: 10,
+                      deleteSpeed: 50,
+                      delay: 60,
+                      cursor: "|",
+                      pauseFor: 1500
                     }}
                   />
                 </h1>
                 <p className="mb-1x">{introdata.description}</p>
+                
+                {introdata.call_to_action && (
+                  <div className="call-to-action mb-3">
+                    <p className="mb-0" style={{ fontWeight: 'bold', color: '#007bff' }}>
+                      {introdata.call_to_action}
+                    </p>
+                  </div>
+                )}
+                
                 <div className="intro_btn-action pb-5">
-                  {/* <Link to="/portfolio" className="text_2">
-                    <div id="button_p" className="ac_btn btn ">
-                      My Portfolio
+                  <Link to="/about">
+                    <div id="button_p" className="ac_btn btn">
+                      About Me
                       <div className="ring one"></div>
                       <div className="ring two"></div>
                       <div className="ring three"></div>
                     </div>
-                  </Link> */}
+                  </Link>
                   <Link to="/contact">
                     <div id="button_h" className="ac_btn btn">
                       Contact Me
